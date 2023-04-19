361b0f0e3f222f1fc1de5359dfff0e8f68144d67
Deividi-Lucas <deividibaby6@gmail.com>
2023-04-18 23:13:11 -0400

    Feat: Dynamic routes

--- main.py
+++ main.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 def home():
     return render_template('home.html')
 
+@app.route('/<user>')
+def user (user):
+    return render_template('user.html', user=user)
+
 if __name__ == '__main__':
     app.run(debug=True)
 
